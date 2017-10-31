dca11b708e2879ec496aa43f81e26f6c2e793757
afzalhmzani <abdullah.alhmzani@gmail.com>
2017-10-31 13:06:01 +0300

    adding Knockout

--- js/app.js
+++ js/app.js
@@ -5,84 +5,90 @@
 // var myFavLocation = function(name, lat, lng){
 //     var 
 // }
+var map;
+var markers =[];
+
+
+var mallsInRiyadh = [
+        
+            {title:'Al Nakheel Mall', location:{lat: 24.7680,lng:46.7146}},
+            {title:'Panorama Mall', location:{lat: 24.6928311,lng:46.6677251}},
+            {title:'Centria Mall', location:{lat: 24.697506,lng:46.6817792}},
+            {title:'Granada Center', location:{lat: 24.7816366,lng:46.7283682}},
+            {title:'Hayat Mall', location:{lat: 24.7432627,lng:46.6783695}},
+        
+        ];
+
+
 
 function initMap() {
     var options = {
-        zoom: 3,
+        zoom: 12,
         center: {
-            lat: 24.7136,
-            lng: 46.6753
+            lat: 24.7680,lng:46.7146
         }
         // center:{lat: -25.363, lng: 131.044}
     };
-
     var map = new google.maps.Map(document.getElementById('map'), options);
-    //24.712461, 46.673398
-    var riyadh = new google.maps.Marker({
-        position: {
-            lat: 24.712461,
-            lng: 46.673398
-        },
+    var infoWindo = new google.maps.InfoWindow();
+    var bounds = new google.maps.LatLngBounds();
+    ko.applyBindings(new AppViewModel()); 
 
-        map: map
-    });
-    var hail = new google.maps.Marker({
-        position: {
-            lat: myFavLocation.Hail.lat,
-            lng: myFavLocation.Hail.lng
-        },
-        map: map
-    });
+    for(var i = 0; i< mallsInRiyadh.length ; i++){
+        var positionOnMap = mallsInRiyadh[i].location; 
+        var title = mallsInRiyadh[i].title; 
 
-    var waukesha = new google.maps.Marker({
-        position: {
-            lat: myFavLocation.Waukesha.lat,
-            lng: myFavLocation.Waukesha.lng
-        },
-        map: map
-    });
+        var marker = new google.maps.Marker({
+            map: map, 
+            position : positionOnMap,
+            title: title,
+            animation: google.maps.Animation.DROP,
+            id: i // No need 
+        });
 
-    var arlington = new google.maps.Marker({
-        position: {
-            lat: myFavLocation.Arlington.lat,
-            lng: myFavLocation.Arlington.lng
-        },
-        map: map
-    });
+        markers.push(marker); 
+        marker.addListener('click', function(){
+            showInfoWindow(this, infoWindo); 
+        });
+
+        bounds.extend(markers[i].position); // might deleteted 
+    }
+    map.fitBounds(bounds); //might deleted
 }
 
-var myFavLocation = {
-    "Hail": {
-        lat: 27.5114,
-        lng: 41.7208
-    },
+function showInfoWindow(marker, infoWindow){
+    if(infoWindow.marker != marker){
+        infoWindow.marker = marker; 
+        infoWindow.setContent('<div>' + marker.title + '</div>'); 
+        infoWindow.open(map,marker); 
 
-    "Waukesha": {
-        lat: 43.0117,
-        lng: -88.2315
-    },
-
-    "Arlington": {
-        lat: 32.700708,
-        lng: -97.124691
+        infoWindow.addListener('closeclick', function(){
+            infoWindow.setMarker = null; 
+        });
     }
+}
 
 
-};
+// Place should be a function that has the info: 
 
 function AppViewModel(){
-    // this.firstName = ko.observable("Bert");
-    // this.lastName = ko.observable("Bertington");
-    // this.fullName = ko.computed(function(){
-    //     return this.firstName() + " " + this.lastName(); 
-    // }, this); 
+    
+   
     var self = this; 
     self.places = ko.observableArray([
-        {name: 'Hail'},
-        {name: 'Riyadh'},
-        {name: 'Waukesha'},
-        {name: 'Arlington'}
-    ]);
+        {title: mallsInRiyadh[0].title},
+        {title: mallsInRiyadh[1].title},
+        {title: mallsInRiyadh[2].title},
+        {title: mallsInRiyadh[3].title},
+        {title: mallsInRiyadh[4].title}
+    ]); 
+
+    self.placeInfo = function(title){
+        google.map.evet.trigger(self.marker, 'click')
+    };
+
+    console.log("----------------"); 
+    console.log(self.placeInfo); 
 }
 
-ko.applyBindings(new AppViewModel());
+// ko.applyBindings(new AppViewModel());
